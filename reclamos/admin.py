696c864a2d00e7d342caffe93bac4b0464762eb2
AF <alfredofernandez@gmail.com>
2025-02-13 18:50:37 -0300

    reclamos model updated to add the user creation + admin module updated

--- reclamos/admin.py
+++ reclamos/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Reclamo
 
-admin.site.register(Reclamo)
+@admin.register(Reclamo)
+class ReclamoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'nombre_apellido', 'fecha', 'autor']
+    search_fields = ['titulo', 'nombre_apellido', 'descripcion']
+    ordering = ['-fecha']
+    raw_id_fields = ['autor']
--- reclamos/models.py
+++ reclamos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Reclamo(models.Model):
@@ -6,6 +7,7 @@ class Reclamo(models.Model):
     nombre_apellido  = models.CharField(max_length=20)
     descripcion = models.TextField(null=True, blank=True)
     fecha = models.DateField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.PROTECT)
     
     def __str__(self):
         return f'{self.titulo} ({self.nombre_apellido})'
