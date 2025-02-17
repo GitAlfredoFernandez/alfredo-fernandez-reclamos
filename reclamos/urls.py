8dc157c39df7e46d927aa74fad1650d826fdd0c7
AF <alfredofernandez@gmail.com>
2025-02-17 20:12:19 -0300

    clases: actualizar detalles eliminar listar

--- reclamos/urls.py
+++ reclamos/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('reclamo-crear/', views.reclamo_crear, name="reclamo_crear"),
     path('reclamo-listar/', views.ReclamoListView.as_view(), name="reclamo_listar"),
+    path('reclamo-detalle/<int:pk>/', views.ReclamoDetailView.as_view(), name="reclamo_detalle"),
+    path('reclamo-eliminar/<int:pk>/', views.ReclamoDeleteView.as_view(), name="reclamo_eliminar"),
+    path('reclamo-actualizar/<int:pk>/', views.ReclamoUpdateView.as_view(), name="reclamo_actualizar"),
 ]
--- reclamos/views.py
+++ reclamos/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from reclamos.models import Reclamo, ReclamoEstado, ReclamoTipo
 from reclamos.forms import ReclamoCrear
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView
 
 def inicio(request):
     # return HttpResponse("<h1>ESTE ES MI PRIMERA VISTA</h1>")
     return render(request, 'reclamos/inicio.html')
 
-def reclamo_crear(request):
+def reclamo_crear(request,instance=None):
     # print(request.GET)
     # print(request.POST)
     formulario = ReclamoCrear()
@@ -27,6 +28,24 @@ def reclamo_crear(request):
             
     return render(request, 'reclamos/reclamo-crear.html', {'formulario': formulario})
 
+class ReclamoDetailView(DetailView):
+    model = Reclamo
+    template_name = 'reclamos/reclamo-detalle.html'
+    context_object_name = 'reclamo'
+    
+class ReclamoDeleteView(DeleteView):
+    model = Reclamo
+    template_name = 'reclamos/reclamo-eliminar.html'
+    context_object_name = 'reclamo'
+    success_url = reverse_lazy('reclamo_listar')
+
+class ReclamoUpdateView(UpdateView):
+    model = Reclamo
+    template_name = 'reclamos/reclamo-actualizar.html'
+    context_object_name = 'reclamo'
+    fields = ['titulo', 'descripcion', 'reclamo_tipo', 'reclamo_estado']
+    success_url = reverse_lazy('reclamo_listar')
+    
 class ReclamoListView(ListView):
     model = Reclamo
     template_name = 'reclamos/reclamo-listar.html'
